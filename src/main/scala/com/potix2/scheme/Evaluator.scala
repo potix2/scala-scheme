d7d808fe7c49d3415698c2d6b1d168c2af9d8e04
Katsunori Kanda <potix2@gmail.com>
2014-03-24 02:03:25 +0900

    Add type testing

--- src/main/scala/com/potix2/scheme/Evaluator.scala
+++ src/main/scala/com/potix2/scheme/Evaluator.scala
@@ -1,13 +1,27 @@
 package com.potix2.scheme
+import scala.reflect.{ClassTag, classTag}
 
 trait Evaluator {
+
   val primitives = List(
     ("+", numericBinop (_ + _)),
     ("-", numericBinop (_ - _)),
     ("*", numericBinop (_ * _)),
     //("/", numericBinop (_ / _)),
-    ("mod", numericBinop (_ % _))
+    ("mod", numericBinop (_ % _)),
+
+    // type primitives
+    ("boolean?", typeTestOp[LispBool]),
+    ("string?", typeTestOp[LispString]),
+    ("symbol?", typeTestOp[LispAtom]),
+    ("number?", typeTestOp[LispInteger])
   )
+
+  def typeTestOp[A <: LispVal: ClassTag]: List[LispVal] => LispVal =
+    (expr: List[LispVal]) => expr match {
+      case h :: Nil => LispBool(classTag[A].runtimeClass.isInstance(h))
+      case _ => LispBool(false) //TODO: wrong number of arguments for
+    }
 
   def numericBinop(op: (Int, Int) => Int): List[LispVal] => LispVal =
     (xs: List[LispVal]) => LispInteger(xs.map(unpackNumber) match {
--- src/test/scala/com/potix2/scheme/EvaluatorSpec.scala
+++ src/test/scala/com/potix2/scheme/EvaluatorSpec.scala
@@ -29,4 +29,35 @@ class EvaluatorSpec extends SpecificationWithJUnit {
       evaluator.eval(LispList(List(LispAtom("*"), LispInteger(2), LispInteger(3)))) must_== LispInteger(6)
     }
   }
+  "type-test boolean?" should {
+    "(boolean? #t)" in { evaluator.eval(makeExpr("boolean?", LispBool(true))) must_== LispBool(true) }
+    "(boolean? #f)" in { evaluator.eval(makeExpr("boolean?", LispBool(false))) must_== LispBool(true) }
+    "(boolean? \"abc\")" in { evaluator.eval(makeExpr("boolean?", LispString("abc"))) must_== LispBool(false) }
+    "(boolean? 1)" in { evaluator.eval(makeExpr("boolean?", LispInteger(1))) must_== LispBool(false) }
+    "(boolean? a)" in { evaluator.eval(makeExpr("boolean?", LispAtom("a"))) must_== LispBool(false) }
+  }
+
+  "type-test string?" should {
+    "(string? #t)" in { evaluator.eval(makeExpr("string?", LispBool(true))) must_== LispBool(false) }
+    "(string? \"abc\")" in { evaluator.eval(makeExpr("string?", LispString("abc"))) must_== LispBool(true) }
+    "(string? 1)" in { evaluator.eval(makeExpr("string?", LispInteger(1))) must_== LispBool(false) }
+    "(string? a)" in { evaluator.eval(makeExpr("string?", LispAtom("a"))) must_== LispBool(false) }
+  }
+
+  "type-test number?" should {
+    "(number? #t)" in { evaluator.eval(makeExpr("number?", LispBool(true))) must_== LispBool(false) }
+    "(number? \"abc\")" in { evaluator.eval(makeExpr("number?", LispString("abc"))) must_== LispBool(false) }
+    "(number? 1)" in { evaluator.eval(makeExpr("number?", LispInteger(1))) must_== LispBool(true) }
+    "(number? a)" in { evaluator.eval(makeExpr("number?", LispAtom("a"))) must_== LispBool(false) }
+  }
+
+  "type-test symbol?" should {
+    "(symbol? #t)" in { evaluator.eval(makeExpr("symbol?", LispBool(true))) must_== LispBool(false) }
+    "(symbol? \"abc\")" in { evaluator.eval(makeExpr("symbol?", LispString("abc"))) must_== LispBool(false) }
+    "(symbol? 1)" in { evaluator.eval(makeExpr("symbol?", LispInteger(1))) must_== LispBool(false) }
+    "(symbol? a)" in { evaluator.eval(makeExpr("symbol?", LispAtom("a"))) must_== LispBool(true) }
+  }
+
+  def makeExpr(sym:String, value: LispVal): LispList =
+    LispList(List(LispAtom(sym), value))
 }
