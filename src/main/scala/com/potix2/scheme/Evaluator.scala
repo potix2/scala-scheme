8e52e3d3fc9095f502aa60f396f6d3757291576e
Katsunori Kanda <potix2@gmail.com>
2014-04-02 02:29:24 +0900

    some changes

--- src/main/scala/com/potix2/scheme/Evaluator.scala
+++ src/main/scala/com/potix2/scheme/Evaluator.scala
@@ -13,6 +13,29 @@ trait Evaluator {
     //("/", numericBinop (_ / _)),
     ("mod", numericBinop (_ % _)),
 
+    ("=", numBoolBinop (_ == _)),
+    ("<", numBoolBinop (_ < _)),
+    (">", numBoolBinop (_ > _)),
+    ("/=", numBoolBinop (_ != _)),
+    (">=", numBoolBinop (_ >= _)),
+    ("<=", numBoolBinop (_ <= _)),
+
+    ("&&", boolBoolBinop (_ && _)),
+    ("||", boolBoolBinop (_ || _)),
+
+    ("string=?", strBoolBinop (_ == _)),
+    ("string<?", strBoolBinop (_ < _)),
+    ("string>?", strBoolBinop (_ > _)),
+    ("string<=?", strBoolBinop (_ <= _)),
+    ("string>=?", strBoolBinop (_ >= _)),
+
+    // conditional expression
+    ("if", ifExpr _),
+
+    // list primitives
+    ("car", carExpr _),
+    ("cdr", cdrExpr _),
+
     // type primitives
     ("boolean?", typeTestOp[LispBool]),
     ("string?", typeTestOp[LispString]),
@@ -32,9 +55,63 @@ trait Evaluator {
     } yield LispInteger(ints.tail.foldLeft(ints.head)(op))
   }
 
+  def numBoolBinop = boolBinop(unpackNumber)(_)
+  def strBoolBinop = boolBinop(unpackString)(_)
+  def boolBoolBinop = boolBinop(unpackBool)(_)
+
+  def boolBinop[A](unpacker: LispVal => ThrowsError[A])(op: (A,A) => Boolean): List[LispVal] => ThrowsError[LispVal] = {
+    case xs@(lhs :: rhs :: Nil) => for {
+      l <- unpacker(lhs)
+      r <- unpacker(rhs)
+    } yield LispBool(op(l, r))
+    case xs => NumArgs(2, xs).left[LispVal]
+  }
+
   def unpackNumber(value: LispVal): ThrowsError[Int] = value match {
     case LispInteger(n) => n.point[ThrowsError]
     case v => TypeMismatch("number", v).left[Int]
+  }
+
+  def unpackString(value: LispVal): ThrowsError[String] = value match {
+    case LispString(v) => v.point[ThrowsError]
+    case LispInteger(v) => v.toString.point[ThrowsError]
+    case LispBool(v) => v.toString.point[ThrowsError]
+    case v => TypeMismatch("string", v).left[String]
+  }
+
+  def unpackBool(value: LispVal): ThrowsError[Boolean] = value match {
+    case LispBool(b) => b.point[ThrowsError]
+    case v => TypeMismatch("boolean", v).left[Boolean]
+  }
+
+  def ifExpr(args: List[LispVal]): ThrowsError[LispVal] = args match {
+    case xs@(pred :: coseq :: alt :: Nil) => for {
+      cond <- eval(pred)
+      result <- cond match {
+        case LispBool(false) => eval(alt)
+        case _ => eval(coseq)
+      }
+    } yield result
+    case xs => NumArgs(3, xs).left[LispVal]
+  }
+
+  def carExpr(args: List[LispVal]): ThrowsError[LispVal] = args match {
+    case xs@(h::Nil) => h match {
+      case xs@(LispList(y :: ys)) => y.point[ThrowsError]
+      case xs@(LispDottedList(y :: ys, value)) => y.point[ThrowsError]
+      case badArg => TypeMismatch("pair", badArg).left[LispVal]
+    }
+    case xs@(h::t) => NumArgs(1, xs).left[LispVal]
+  }
+
+  def cdrExpr(args: List[LispVal]): ThrowsError[LispVal] = args match {
+    case xs@(h::Nil) => h match {
+      case xs@(LispList(y :: ys)) => LispList(ys).point[ThrowsError]
+      case xs@(LispDottedList(y :: Nil, value)) => value.point[ThrowsError]
+      case xs@(LispDottedList(y :: ys, value)) => LispDottedList(ys, value).point[ThrowsError]
+      case badArg => TypeMismatch("pair", badArg).left[LispVal]
+    }
+    case xs@(h::t) => NumArgs(1, xs).left[LispVal]
   }
 
   def lispApply(sym: String, args: List[LispVal]): ThrowsError[LispVal] = primitives.
@@ -49,10 +126,10 @@ trait Evaluator {
         a <- argList
         result <- lispApply(func, a)
       } yield result
-
     case s:LispString => s.point[ThrowsError]
     case n:LispNumber => n.point[ThrowsError]
     case b:LispBool   => b.point[ThrowsError]
+    case a:LispAtom   => a.point[ThrowsError]
     case v => BadSpecialForm("Unrecognized special form", v).left[LispVal]
   }
 }
--- src/test/scala/com/potix2/scheme/EvaluatorSpec.scala
+++ src/test/scala/com/potix2/scheme/EvaluatorSpec.scala
@@ -1,63 +1,89 @@
 package com.potix2.scheme
 
 import org.specs2.mutable.SpecificationWithJUnit
+import scalaz.Scalaz._
+import com.potix2.scheme.LispError.ThrowsError
 
 class EvaluatorSpec extends SpecificationWithJUnit {
   val evaluator = new Evaluator {}
 
   "eval an atom" should {
     "return the atom" in {
-      evaluator.eval(LispAtom("abcd")) must_== LispAtom("abcd")
+      evaluator.eval(LispAtom("abcd")) must_== LispAtom("abcd").point[ThrowsError]
     }
   }
   "eval a quoted value" should {
-    "return the unquoted value" in {
-      evaluator.eval(LispList(List(LispAtom("quote"), LispAtom("abcd")))) must_== LispAtom("abcd")
+    "'abcd to abcd" in {
+      evaluator.eval(makeExpr("quote", LispAtom("abcd"))).toEither must beRight(LispAtom("abcd"))
+    }
+    "''a to 'a" in {
+      evaluator.eval(makeExpr("quote", makeExpr("quote", LispAtom("a")))).toEither must beRight(LispList(List(LispAtom("quote"), LispAtom("a"))))
     }
   }
   "eval" should {
     "reduce (+ 1 2 3) to 6" in {
-      evaluator.eval(LispList(List(LispAtom("+"), LispInteger(1), LispInteger(2), LispInteger(3)))) must_== LispInteger(6)
+      evaluator.eval(LispList(List(LispAtom("+"), LispInteger(1), LispInteger(2), LispInteger(3)))) must_== LispInteger(6).point[ThrowsError]
     }
     "reduce (- 4 1 2) to 1" in {
-      evaluator.eval(LispList(List(LispAtom("-"), LispInteger(4), LispInteger(1), LispInteger(2)))) must_== LispInteger(1)
+      evaluator.eval(LispList(List(LispAtom("-"), LispInteger(4), LispInteger(1), LispInteger(2)))) must_== LispInteger(1).point[ThrowsError]
     }
     "reduce (+ 2 (- 4 1)) to 5" in {
-      evaluator.eval(LispList(List(LispAtom("+"), LispInteger(2), LispList(List(LispAtom("-"), LispInteger(4), LispInteger(1)))))) must_== LispInteger(5)
+      evaluator.eval(LispList(List(LispAtom("+"), LispInteger(2), LispList(List(LispAtom("-"), LispInteger(4), LispInteger(1)))))) must_== LispInteger(5).point[ThrowsError]
     }
     "reduce (* 2 3) to 6" in {
-      evaluator.eval(LispList(List(LispAtom("*"), LispInteger(2), LispInteger(3)))) must_== LispInteger(6)
+      evaluator.eval(LispList(List(LispAtom("*"), LispInteger(2), LispInteger(3)))) must_== LispInteger(6).point[ThrowsError]
     }
   }
   "type-test boolean?" should {
-    "(boolean? #t)" in { evaluator.eval(makeExpr("boolean?", LispBool(true))) must_== LispBool(true) }
-    "(boolean? #f)" in { evaluator.eval(makeExpr("boolean?", LispBool(false))) must_== LispBool(true) }
-    "(boolean? \"abc\")" in { evaluator.eval(makeExpr("boolean?", LispString("abc"))) must_== LispBool(false) }
-    "(boolean? 1)" in { evaluator.eval(makeExpr("boolean?", LispInteger(1))) must_== LispBool(false) }
-    "(boolean? a)" in { evaluator.eval(makeExpr("boolean?", LispAtom("a"))) must_== LispBool(false) }
+    "(boolean? #t)" in { evaluator.eval(makeExpr("boolean?", LispBool(true))) must_== LispBool(true).point[ThrowsError] }
+    "(boolean? #f)" in { evaluator.eval(makeExpr("boolean?", LispBool(false))) must_== LispBool(true).point[ThrowsError] }
+    "(boolean? \"abc\")" in { evaluator.eval(makeExpr("boolean?", LispString("abc"))) must_== LispBool(false).point[ThrowsError] }
+    "(boolean? 1)" in { evaluator.eval(makeExpr("boolean?", LispInteger(1))) must_== LispBool(false).point[ThrowsError] }
+    "(boolean? a)" in { evaluator.eval(makeExpr("boolean?", LispAtom("a"))) must_== LispBool(false).point[ThrowsError] }
   }
 
   "type-test string?" should {
-    "(string? #t)" in { evaluator.eval(makeExpr("string?", LispBool(true))) must_== LispBool(false) }
-    "(string? \"abc\")" in { evaluator.eval(makeExpr("string?", LispString("abc"))) must_== LispBool(true) }
-    "(string? 1)" in { evaluator.eval(makeExpr("string?", LispInteger(1))) must_== LispBool(false) }
-    "(string? a)" in { evaluator.eval(makeExpr("string?", LispAtom("a"))) must_== LispBool(false) }
+    "(string? #t)" in { evaluator.eval(makeExpr("string?", LispBool(true))) must_== LispBool(false).point[ThrowsError] }
+    "(string? \"abc\")" in { evaluator.eval(makeExpr("string?", LispString("abc"))) must_== LispBool(true).point[ThrowsError] }
+    "(string? 1)" in { evaluator.eval(makeExpr("string?", LispInteger(1))) must_== LispBool(false).point[ThrowsError] }
+    "(string? a)" in { evaluator.eval(makeExpr("string?", LispAtom("a"))) must_== LispBool(false).point[ThrowsError] }
   }
 
   "type-test number?" should {
-    "(number? #t)" in { evaluator.eval(makeExpr("number?", LispBool(true))) must_== LispBool(false) }
-    "(number? \"abc\")" in { evaluator.eval(makeExpr("number?", LispString("abc"))) must_== LispBool(false) }
-    "(number? 1)" in { evaluator.eval(makeExpr("number?", LispInteger(1))) must_== LispBool(true) }
-    "(number? a)" in { evaluator.eval(makeExpr("number?", LispAtom("a"))) must_== LispBool(false) }
+    "(number? #t)" in { evaluator.eval(makeExpr("number?", LispBool(true))) must_== LispBool(false).point[ThrowsError] }
+    "(number? \"abc\")" in { evaluator.eval(makeExpr("number?", LispString("abc"))) must_== LispBool(false).point[ThrowsError] }
+    "(number? 1)" in { evaluator.eval(makeExpr("number?", LispInteger(1))) must_== LispBool(true).point[ThrowsError] }
+    "(number? a)" in { evaluator.eval(makeExpr("number?", LispAtom("a"))) must_== LispBool(false).point[ThrowsError] }
   }
 
   "type-test symbol?" should {
-    "(symbol? #t)" in { evaluator.eval(makeExpr("symbol?", LispBool(true))) must_== LispBool(false) }
-    "(symbol? \"abc\")" in { evaluator.eval(makeExpr("symbol?", LispString("abc"))) must_== LispBool(false) }
-    "(symbol? 1)" in { evaluator.eval(makeExpr("symbol?", LispInteger(1))) must_== LispBool(false) }
-    "(symbol? a)" in { evaluator.eval(makeExpr("symbol?", LispAtom("a"))) must_== LispBool(true) }
+    "(symbol? #t)" in { evaluator.eval(makeExpr("symbol?", LispBool(true))) must_== LispBool(false).point[ThrowsError] }
+    "(symbol? \"abc\")" in { evaluator.eval(makeExpr("symbol?", LispString("abc"))) must_== LispBool(false).point[ThrowsError] }
+    "(symbol? 1)" in { evaluator.eval(makeExpr("symbol?", LispInteger(1))) must_== LispBool(false).point[ThrowsError] }
+    "(symbol? a)" in { evaluator.eval(makeExpr("symbol?", LispAtom("a"))) must_== LispBool(true).point[ThrowsError] }
   }
 
-  def makeExpr(sym:String, value: LispVal): LispList =
-    LispList(List(LispAtom(sym), value))
+  "if" should {
+    "(if #f 1 2)" in { evaluator.eval(makeExpr("if", LispBool(false), LispInteger(1), LispInteger(2))).toEither must beRight(LispInteger(2)) }
+    "(if #f 1 2)" in { evaluator.eval(makeExpr("if", LispBool(true), LispInteger(1), LispInteger(2))).toEither must beRight(LispInteger(1)) }
+  }
+
+  "car" should {
+    "(car 'a)" in { evaluator.eval(makeExpr("car", LispList(List(LispAtom("quote"), LispAtom("a"))))).toEither must beLeft(TypeMismatch("pair", LispAtom("a"))) }
+    "(car 'a 'b)" in { evaluator.eval(makeExpr("car", makeExpr("quote", LispAtom("a")), makeExpr("quote", LispAtom("b")))).toEither must beLeft(beAnInstanceOf[NumArgs]) }
+    "(car '(a b))" in { evaluator.eval(makeExpr("car", makeExpr("quote", LispList(List(LispAtom("a"), LispAtom("b")))))).toEither must beRight(LispAtom("a")) }
+    "(car '(a b . c))" in { evaluator.eval(makeExpr("car", makeExpr("quote", LispDottedList(List(LispAtom("a"), LispAtom("b")),LispAtom("c"))))).toEither must beRight(LispAtom("a")) }
+  }
+
+  "cdr" should {
+    "(cdr 'a)" in { evaluator.eval(makeExpr("cdr", LispList(List(LispAtom("quote"), LispAtom("a"))))).toEither must beLeft(TypeMismatch("pair", LispAtom("a"))) }
+    "(cdr 'a 'b)" in { evaluator.eval(makeExpr("cdr", makeExpr("quote", LispAtom("a")), makeExpr("quote", LispAtom("b")))).toEither must beLeft(beAnInstanceOf[NumArgs]) }
+    "(cdr '(a b))" in { evaluator.eval(makeExpr("cdr", makeExpr("quote", LispList(List(LispAtom("a"), LispAtom("b")))))).toEither must beRight(LispList(List(LispAtom("b")))) }
+    "(car '(a . b))" in { evaluator.eval(makeExpr("cdr", makeExpr("quote", LispDottedList(List(LispAtom("a")),LispAtom("b"))))).toEither must beRight(LispAtom("b")) }
+    "(car '(a b . c))" in { evaluator.eval(makeExpr("cdr", makeExpr("quote", LispDottedList(List(LispAtom("a"), LispAtom("b")),LispAtom("c"))))).toEither must beRight(LispDottedList(List(LispAtom("b")), LispAtom("c"))) }
+    "(cdr '(a))" in { evaluator.eval(makeExpr("cdr", makeExpr("quote", LispList(List(LispAtom("a")))))).toEither must beRight(LispList(List())) }
+  }
+
+  def makeExpr(sym:String, values: LispVal*): LispList =
+    LispList((LispAtom(sym) :: values.toList))
 }
